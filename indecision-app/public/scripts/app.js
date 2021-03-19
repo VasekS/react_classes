1fdc9fb259d69b5c50454ee641225448e0b7c800
Vasek Sekret <vasik.sekret@gmail.com>
2021-03-18 23:11:07 -0700

    Challenge

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -22,3 +22,26 @@ var user = {
 	}
 };
 console.log(user.printPlacesLived());
+
+//Challenge area
+
+var multiplier = {
+	//numbers - array of numbers that we want to multiply
+	numbers: [10, 20, 30],
+	multiplyBy: 5,
+	multiply: function multiply() {
+		var _this2 = this;
+
+		return this.numbers.map(function (newNumbers) {
+			return newNumbers * _this2.multiplyBy;
+		});
+		//{
+		// return this.multiplyBy * newNumbers;
+		// });
+	}
+	//multiplyBy - single number
+	//method multiply: will terurn a new array where the number have been multiplied
+
+};
+
+console.log(multiplier.multiply()); // [1, 2, 3] 2 [2, 4, 6]
