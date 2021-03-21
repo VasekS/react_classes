c74045d43d26889a28c5cc760fa8bdd2c096b4cf
Vasek Sekret <vasik.sekret@gmail.com>
2021-03-20 17:30:53 -0700

    testing

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -18,25 +18,12 @@ const template = (
 	</div>
 );
 
-const user = {
-	name: 'Andrew',
-	age: 26,
-	location: 'Los Angeles'
-};
-function getLocation(location) {
-	if (location) {
-		return <p>Location: {location}</p>;
-	}
-}
-
-const templateTwo = (
+const templateTwo = {
 	<div>
-		<h1>{user.name ? user.name : 'Anonymus'}</h1>
-		{(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-		{getLocation(user.location)}
+		<h1>Count: {}</h1>
 	</div>
-);
+};
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
