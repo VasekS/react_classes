46b44a4ba38890098d9988c7fe8343942d94db90
Vasek Sekret <vasik.sekret@gmail.com>
2021-03-23 23:10:39 -0700

    Chapter 3 Challenge - Visibility Button

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,36 +1,75 @@
 'use strict';
 
-console.log('App.js is running!');
+var visibility = false;
 
-var app = {
-	title: 'Visibility Toggle',
-	subtitle: 'Hey. There are some details you can now see!',
-	options: []
+var toggleVisibility = function toggleVisibility() {
+	visibility = !visibility;
+	renderVisibilityApp();
 };
-
-var onShowDetails = function onShowDetails() {
-	console.log('its working');
-	React.createElement(
-		'p',
-		null,
-		'app.subtitle'
-	);
-};
-
-var visibilityToggle = React.createElement(
-	'div',
-	null,
-	React.createElement(
-		'h1',
-		null,
-		app.title
-	),
-	React.createElement(
-		'button',
-		{ onClick: onShowDetails },
-		'Show Details'
-	)
-);
 
 var appRoot = document.getElementById('app');
-ReactDOM.render(visibilityToggle, appRoot);
+
+var renderVisibilityApp = function renderVisibilityApp() {
+	var visibilityToggle = React.createElement(
+		'div',
+		null,
+		React.createElement(
+			'h1',
+			null,
+			'Visibility App'
+		),
+		React.createElement(
+			'button',
+			{ onClick: toggleVisibility },
+			visibility ? 'Hide details' : 'Show Details'
+		),
+		visibility && React.createElement(
+			'div',
+			null,
+			React.createElement(
+				'p',
+				null,
+				'Hey. These are some details here!'
+			)
+		)
+	);
+
+	ReactDOM.render(visibilityToggle, appRoot);
+};
+
+renderVisibilityApp();
+
+//MY SOLUTION
+// console.log('App.js is running!');
+// 
+// 
+// const app = {
+// 	title: 'Visibility Toggle'
+// };
+// 
+// let buttonBool = false;
+// 
+// const onShowDetails = () => {
+// 	console.log('its working');
+// 	if (buttonBool === false) {
+// 		buttonBool = true;
+// 	} else {
+// 		buttonBool = false;
+// 	}
+// 	renderVisibilityApp();
+// };
+// 
+// const appRoot = document.getElementById('app');
+// 
+//  const renderVisibilityApp = () => {
+// 	const visibilityToggle = (
+// 		<div>
+// 			<h1>{app.title}</h1>
+// 			<button onClick={onShowDetails}>{buttonBool === false ? 'Show Details' : 'Hide Details'}</button>
+// 			<p>{buttonBool === true ? 'Hey. There are some details you can now see!' : ' '}</p>
+// 		</div>
+// 	);
+//  	ReactDOM.render(visibilityToggle, appRoot);
+//  };
+// 
+//  renderVisibilityApp();
--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -1,23 +1,63 @@
-console.log('App.js is running!');
+let visibility = false;
 
-const app = {
-	title: 'Visibility Toggle',
-	subtitle: 'Hey. There are some details you can now see!',
-	options: []
+const toggleVisibility = () => {
+	visibility = !visibility;
+	renderVisibilityApp();
 };
-
-const onShowDetails = () => {
-	console.log('its working');
-	<p>app.subtitle</p>;
-};
-
-const visibilityToggle = (
-	<div>
-		<h1>{app.title}</h1>
-		<button onClick={onShowDetails}>Show Details</button>
-
-	</div>
-	);
 
 const appRoot = document.getElementById('app');
-ReactDOM.render(visibilityToggle, appRoot);
+
+const renderVisibilityApp = () => {
+	const visibilityToggle = (
+		<div>
+			<h1>Visibility App</h1>
+			<button onClick={toggleVisibility}>
+				{visibility ? 'Hide details' : 'Show Details'}	
+			</button>
+			{visibility && (
+				<div>
+					<p>Hey. These are some details here!</p>
+				</div>
+			)}
+		</div>
+	);
+
+	ReactDOM.render(visibilityToggle, appRoot);
+};
+
+renderVisibilityApp();
+
+//MY SOLUTION
+// console.log('App.js is running!');
+// 
+// 
+// const app = {
+// 	title: 'Visibility Toggle'
+// };
+// 
+// let buttonBool = false;
+// 
+// const onShowDetails = () => {
+// 	console.log('its working');
+// 	if (buttonBool === false) {
+// 		buttonBool = true;
+// 	} else {
+// 		buttonBool = false;
+// 	}
+// 	renderVisibilityApp();
+// };
+// 
+// const appRoot = document.getElementById('app');
+// 
+//  const renderVisibilityApp = () => {
+// 	const visibilityToggle = (
+// 		<div>
+// 			<h1>{app.title}</h1>
+// 			<button onClick={onShowDetails}>{buttonBool === false ? 'Show Details' : 'Hide Details'}</button>
+// 			<p>{buttonBool === true ? 'Hey. There are some details you can now see!' : ' '}</p>
+// 		</div>
+// 	);
+//  	ReactDOM.render(visibilityToggle, appRoot);
+//  };
+// 
+//  renderVisibilityApp();
