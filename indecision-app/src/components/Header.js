738a6b36624f82384109add9519adf816c5783bf
Vasek Sekret <vasik.sekret@gmail.com>
2021-04-07 09:18:21 -0700

    Adding Components

---
+++ indecision-app/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//flexible header component
+const Header = (props) => {
+	return (
+		<div>
+			<h1>{props.title}</h1>
+			{props.subtitle && <h2>{props.subtitle}</h2>}
+		</div>
+	);
+};
+
+Header.defaultProps = {
+	title: 'Indecision'
+};
+
+export default Header;
