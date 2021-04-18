0cc0b595986ca136e47b6af788f07967a7c641ab
Vasek Sekret <vasik.sekret@gmail.com>
2021-04-17 21:17:47 -0700

    adding components

---
+++ portfolio-site/src/components/PortfolioDashboardPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PortfolioDashboardpage = () => (
+	<div>
+		This if from Dashboard!
+	</div>
+);
+
+export default PortfolioDashboardpage;
--- portfolio-site/src/routers/AppRouter.js
+++ portfolio-site/src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import Header from '../components/Header';
+import PortfolioDashboardpage from '../components/PortfolioDashboardpage';
 import NotFoundPage from '../components/NotFoundPage';
 
 
@@ -9,6 +10,7 @@ const AppRouter = () => (
 		<div>
 			<Header />
 				<Switch>
+				<Route path="/"component={PortfolioDashboardpage} />
 				<Route component={NotFoundPage} />
 			</Switch>
 		</div>
