fe16cec528da0de26476146fdc31f6082750c1c6
Vasek Sekret <vasik.sekret@gmail.com>
2021-04-22 08:13:00 -0700

    Adding spread operator

--- expensify-app/src/playground/redux-expensify.js
+++ expensify-app/src/playground/redux-expensify.js
@@ -1,7 +1,30 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
 
 // ADD_EXPENSE
+const addExpense = (
+	{ 
+		description = '', 
+		note = '', 
+		amount = 0, 
+		createdAt = 0 } = {}
+) => ({
+	type: 'ADD_EXPENSE',
+	expense: {
+		id: uuid(),
+		description,
+		note,
+		amount,
+		createdAt
+	}
+});
+
 // REMOVE_EXPENSE
+
+const removeExpense = ({ id } = {}) => ({
+	type: 'REMOVE_EXPENSE',
+	id
+});
 // EDIT_EXPENSE
 // SET_TEXT_FILTER
 // SORT_BY_DATE
@@ -10,18 +33,23 @@ import { createStore, combineReducers } from 'redux';
 // SET_END_DATE
 
 // Expenses reducers
-
 const expenseReducerDefaultState = [];
 
 const expenseReducer = (state = expenseReducerDefaultState, action) => {
 	switch (action.type) {
+		case 'ADD_EXPENSE':
+			return [
+				...state,
+				action.expense
+			];
+		case 'REMOVE_EXPENSE':
+			return state.filter(({ id }) => id !== action.id);
 		default:
 			return state;
 	}
 };
 
 // Filters reducer
-
 const filtersRedurerDefaultState = {
 	text: '',
 	sortBy: 'date',
@@ -37,7 +65,6 @@ const filtersReducer = (state = filtersRedurerDefaultState, action) => {
 };
 
 // Store creation
-
 const store = createStore(
 	combineReducers({
 		expenses: expenseReducer,
@@ -45,7 +72,15 @@ const store = createStore(
 	})
 );
 
-console.log(store.getState());
+store.subscribe(() => {
+	console.log(store.getState());
+});
+
+const expenseOne = store.dispatch(addExpense({ description: 'HOA', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+
+store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
 
 const demoState = {
 	expenses: [{
