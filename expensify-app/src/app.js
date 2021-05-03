3f0218bb7efa4711a8bc90d91c26b7e7a41b583b
Vasek Sekret <vasik.sekret@gmail.com>
2021-05-02 20:19:52 -0700

    Challenge part 3

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -2,18 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import addExpense from './actions/expenses';
+import { addExpense } from './actions/expenses';
+import { setTextFilter } from './actions/filters';
+import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
 const store = configureStore();
 
-//Challenge
-// const newExpense = addExpense({description: 'Water'});
-const expenseOne = store.dispatch(addExpense({ description: 'HOA', amount: 100, createdAt: -21000 }));
-
-console.log(expenseOne());
+store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Gas bill' }));
 
 console.log(store.getState());
 
