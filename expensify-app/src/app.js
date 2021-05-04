e1878bb5da966526a9e0f1315024b97ecddda88a
Vasek Sekret <vasik.sekret@gmail.com>
2021-05-03 18:34:25 -0700

    Challenge part 4

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -14,6 +14,12 @@ const store = configureStore();
 store.dispatch(addExpense({ description: 'Water bill' }));
 store.dispatch(addExpense({ description: 'Gas bill' }));
 
+store.dispatch(setTextFilter({ text: 'bill' }));
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
+
 console.log(store.getState());
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
