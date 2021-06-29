03e53f6e62931650fcf05eb8772663833362d8e9
Vasek Sekret <vasik.sekret@gmail.com>
2021-06-28 22:51:52 -0700

    higher order component

---
+++ expensify-app/src/playground/hoc.js
@@ -0,0 +1,28 @@
+//Higher Order Components (HOC) - A component (HOC) that renders another component
+// Goal - reuse code
+// Render hijacking
+// Prop manipulation
+// Abstract state
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Info = (props) => (
+	<div>
+		<h1>Info</h1>
+		<p>The info is: {props.info}</p>
+	</div>
+);
+
+const withAdminWarning = (WrappedComponent) => {
+	return (props) => (
+		<div>
+			<p>This is private info. Please don't share!</p>
+			<WrappedComponent />
+		</div>
+	);
+};
+
+const AdminInfo = withAdminWarning(Info);
+
+ReactDOM.render(<AdminInfo info="This are the details" />, document.getElementById('app'));
