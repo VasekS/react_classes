9f21d9781c8dfb9e10f2c5babde59f430ebc84cb
Vasek Sekret <vasik.sekret@gmail.com>
2021-09-14 18:12:30 -0700

    Added select filter input

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -15,11 +15,6 @@ const store = configureStore();
 store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
 store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
 store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-store.dispatch(setTextFilter({ text: 'water' }));
-
-// setTimeout(() => {
-// 	store.dispatch(setTextFilter({ text: 'rent' }));
-// }, 3000)
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
